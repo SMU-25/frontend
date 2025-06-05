115dd7052b101d2fc4ecd8181a02aed6bf7d57af
choi0510 <yh733609@gmail.com>
2025-06-05 17:17:19 +0900

    🚀 Chore: Flutter 프로젝트 초기 세팅 (색상, 폰트, 이미지 포함)

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 주색상
+const MAIN_COLOR = Color(0xFF64CCC5);
+// 미열 색상
+const MILD_FEVER_COLOR = Color(0xFFFBBC05);
+// 고열 색상
+const HIGH_FEVER_COLOR = Color(0xFFF2665B);
+// 아이콘 회색 색상
+const ICON_GREY_COLOR = Color(0xFFD9D9D9);
