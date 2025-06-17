ba1c0d89d70b91ca59bb595b7aa02a147bb0f331
Layton <jihwan8807@naver.com>
2025-06-18 03:02:33 +0900

    ✨ [Feat] 로그인 페이지 UI 구현

---
+++ lib/common/component/navigation_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NavigationButton extends StatelessWidget {
+  const NavigationButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff64CCC5),
+        padding: EdgeInsets.symmetric(horizontal: 150, vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
