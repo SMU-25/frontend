9742399fea3a68aa2103ac1ab17d2700dbb9da7a
choi0510 <yh733609@gmail.com>
2025-06-21 13:49:22 +0900

    ✨ [Feat] 설정의 로그아웃, 회원 탈퇴 모달 창 UI 구현

--- lib/settings/view/settings_screen.dart
+++ lib/settings/view/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:team_project_front/common/component/yes_or_no_dialog.dart';
 import 'package:team_project_front/common/const/colors.dart';
 import 'package:team_project_front/settings/component/custom_appbar.dart';
 import 'package:team_project_front/settings/view/notification_settings_screen.dart';
@@ -13,8 +14,8 @@ class SettingsScreen extends StatelessWidget {
       {'title': '알림 설정', 'onTap': () => onPressedNotification(context)},
       {'title': '약관', 'onTap': () => onPressedTerms(context)},
       {'title': '버전 정보', 'trailing': '1.0.0', 'onTap': null},
-      {'title': '로그아웃', 'onTap': () {}},
-      {'title': '회원 탈퇴', 'onTap': () {}},
+      {'title': '로그아웃', 'onTap': () => onPressedLogout(context)},
+      {'title': '회원 탈퇴', 'onTap': () => onPressedWithdraw(context)},
     ];
 
     return Scaffold(
@@ -39,6 +40,39 @@ class SettingsScreen extends StatelessWidget {
   void onPressedNotification(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (context) => NotificationSettingsScreen())
+    );
+  }
+
+  void onPressedLogout(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => YesOrNoDialog(
+        title: '로그아웃',
+        content: '정말 로그아웃 하시겠습니까?',
+        onPressedYes: () {
+          // 로그아웃 처리 로직
+        },
+        yesText: '예',
+        noText: '아니오',
+      ),
+    );
+  }
+
+  void onPressedWithdraw(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => YesOrNoDialog(
+        title: '회원 탈퇴',
+        content:
+          '회원 탈퇴 시 기록하신 모든 데이터가\n'
+          '삭제되어 복구가 불가능합니다.\n'
+          '정말 탈퇴하시겠습니까?',
+        onPressedYes: () {
+          // 회원 탈퇴 처리 로직
+        },
+        yesText: '예',
+        noText: '아니오',
+      ),
     );
   }
 }
