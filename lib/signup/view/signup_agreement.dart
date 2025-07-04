746b2c2ae293acd71e9beef2edfd9df1c0561aae
Layton <jihwan8807@naver.com>
2025-07-05 04:56:38 +0900

    ✨ [Refactor] Remove Stack in SignupAgreementScreen \

--- lib/signup/view/signup_agreement.dart
+++ lib/signup/view/signup_agreement.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:team_project_front/common/component/custom_checkbox.dart';
 import 'package:team_project_front/common/component/navigation_button.dart';
+import 'package:team_project_front/signup/view/signup_email.dart';
 
 class SignupAgreementScreen extends StatefulWidget {
   const SignupAgreementScreen({super.key});
@@ -29,91 +30,103 @@ class _SignupAgreementScreenState extends State<SignupAgreementScreen> {
     });
   }
 
+  bool _isAgreementValid() {
+    return ageAgreed &&
+        personalInfoAgreed &&
+        sensitiveInfoAgreed &&
+        locationAgreed;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('회원가입', style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  '맘편해 서비스 이용약관에\n동의해주세요',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                SizedBox(height: 25),
-                CustomCheckbox(
-                  text: '전체 동의',
-                  size: 20,
-                  value: allAgreed,
-                  onChanged: toggleAll,
-                  showArrow: false,
-                ),
-                SizedBox(height: 15),
-                Divider(),
-                SizedBox(height: 15),
-                CustomCheckbox(
-                  text: '[필수] 만 14세 이상입니다.',
-                  size: 15,
-                  value: ageAgreed,
-                  onChanged: (val) => setState(() => ageAgreed = val!),
-                  showArrow: false,
-                ),
-                SizedBox(height: 15),
-                CustomCheckbox(
-                  text: '[필수] 개인정보 수집 및 이용동의',
-                  size: 15,
-                  value: personalInfoAgreed,
-                  onChanged: (val) => setState(() => personalInfoAgreed = val!),
-                  showArrow: true,
-                ),
-                SizedBox(height: 15),
-                CustomCheckbox(
-                  text: '[필수] 민감정보 수집 및 이용동의',
-                  size: 15,
-                  value: sensitiveInfoAgreed,
-                  onChanged:
-                      (val) => setState(() => sensitiveInfoAgreed = val!),
-                  showArrow: true,
-                ),
-                SizedBox(height: 15),
-                CustomCheckbox(
-                  text: '[필수] 위치기반 서비스 이용약관',
-                  size: 15,
-                  value: locationAgreed,
-                  onChanged: (val) => setState(() => locationAgreed = val!),
-                  showArrow: true,
-                ),
-                SizedBox(height: 15),
-                CustomCheckbox(
-                  text: '[선택] 광고성 수신 동의',
-                  size: 15,
-                  value: adAgreed,
-                  onChanged: (val) => setState(() => adAgreed = val!),
-                  showArrow: true,
-                ),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '맘편해 서비스 이용약관에\n동의해주세요',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 50,
-            child: Center(
-              child: NavigationButton(
-                text: '다음',
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-              ),
+            SizedBox(height: 25),
+            CustomCheckbox(
+              text: '전체 동의',
+              size: 20,
+              value: allAgreed,
+              onChanged: toggleAll,
+              showArrow: false,
             ),
+            SizedBox(height: 15),
+            Divider(),
+            SizedBox(height: 15),
+            CustomCheckbox(
+              text: '[필수] 만 14세 이상입니다.',
+              size: 15,
+              value: ageAgreed,
+              onChanged: (val) => setState(() => ageAgreed = val!),
+              showArrow: false,
+            ),
+            SizedBox(height: 15),
+            CustomCheckbox(
+              text: '[필수] 개인정보 수집 및 이용동의',
+              size: 15,
+              value: personalInfoAgreed,
+              onChanged: (val) => setState(() => personalInfoAgreed = val!),
+              showArrow: true,
+            ),
+            SizedBox(height: 15),
+            CustomCheckbox(
+              text: '[필수] 민감정보 수집 및 이용동의',
+              size: 15,
+              value: sensitiveInfoAgreed,
+              onChanged: (val) => setState(() => sensitiveInfoAgreed = val!),
+              showArrow: true,
+            ),
+            SizedBox(height: 15),
+            CustomCheckbox(
+              text: '[필수] 위치기반 서비스 이용약관',
+              size: 15,
+              value: locationAgreed,
+              onChanged: (val) => setState(() => locationAgreed = val!),
+              showArrow: true,
+            ),
+            SizedBox(height: 15),
+            CustomCheckbox(
+              text: '[선택] 광고성 수신 동의',
+              size: 15,
+              value: adAgreed,
+              onChanged: (val) => setState(() => adAgreed = val!),
+              showArrow: true,
+            ),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 50),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: NavigationButton(
+            text: '다음',
+            onPressed: () {
+              if (_isAgreementValid()) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => SignupEmailScreen()),
+                );
+              } else {
+                // snackBar로 필수항목 동의 메시지 처리함
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text('필수 항목에 모두 동의해주세요.')));
+              }
+            },
           ),
-        ],
+        ),
       ),
     );
   }
