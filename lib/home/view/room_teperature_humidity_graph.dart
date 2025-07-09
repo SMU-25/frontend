c2fc33b3a10bbfc011fb16f85c9c7942e365182a
Layton <jihwan8807@naver.com>
2025-07-10 00:58:41 +0900

    ✨ [Feat] Add Body Temperature and Room Temperature/Humidity Graph Screens with date selection button

---
+++ lib/home/view/room_teperature_humidity_graph.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:team_project_front/common/component/temperature_chart_widget.dart';
+
+class RoomTemperatureHumidityGraphScreen extends StatefulWidget {
+  const RoomTemperatureHumidityGraphScreen({super.key});
+
+  @override
+  State<RoomTemperatureHumidityGraphScreen> createState() =>
+      _RoomTemperatureHumidityGraphScreenState();
+}
+
+class _RoomTemperatureHumidityGraphScreenState
+    extends State<RoomTemperatureHumidityGraphScreen> {
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2006),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final String selectedDateText = DateFormat(
+      'yyyy년 MM월 dd일',
+    ).format(selectedDate);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '방 온도/습도 그래프',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => _selectDate(context),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
+                textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(selectedDateText),
+                  SizedBox(width: 4),
+                  Icon(Icons.arrow_drop_down),
+                ],
+              ),
+            ),
+
+            _ChartSectionWidget(chartType: ChartType.roomTemp),
+            _ChartSectionWidget(chartType: ChartType.humidity),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ChartSectionWidget extends StatelessWidget {
+  const _ChartSectionWidget({required this.chartType});
+
+  final ChartType chartType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: TemperatureChartWidget(chartType: chartType),
+    );
+  }
+}
