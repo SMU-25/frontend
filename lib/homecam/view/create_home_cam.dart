92240e587ebebf96c31e41480395ecced7a224dd
Layton <jihwan8807@naver.com>
2025-07-10 05:02:28 +0900

    ✨ [Feat] Add CreateHomeCamScreen for home camera registration

---
+++ lib/homecam/view/create_home_cam.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:team_project_front/common/view/root_tab.dart';
+import 'package:team_project_front/homecam/component/home_cam_form.dart';
+import 'package:team_project_front/homecam/view/create_home_cam_complete.dart';
+import 'package:team_project_front/common/component/custom_navigation_bar.dart';
+
+class CreateHomeCamScreen extends StatefulWidget {
+  const CreateHomeCamScreen({super.key});
+
+  @override
+  State<CreateHomeCamScreen> createState() => _CreateHomeCamScreenState();
+}
+
+class _CreateHomeCamScreenState extends State<CreateHomeCamScreen> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController installationPlaceController =
+      TextEditingController();
+  final TextEditingController homeCamSerialNumController =
+      TextEditingController();
+  String? selectedChild;
+  final List<String> children = ['준형', '지환', '강민'];
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    installationPlaceController.dispose();
+    homeCamSerialNumController.dispose();
+    super.dispose();
+  }
+
+  void onChildChanged(String? value) {
+    setState(() {
+      selectedChild = value;
+    });
+  }
+
+  void onSubmit() {
+    if (formKey.currentState!.validate() && selectedChild != null) {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (_) => const CreateHomeCamCompleteScreen()),
+      );
+    } else if (selectedChild == null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('아이를 선택해주세요')));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '홈캠 등록',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: HomeCamForm(
+        formKey: formKey,
+        nameController: nameController,
+        installationPlaceController: installationPlaceController,
+        serialNumController: homeCamSerialNumController,
+        selectedChild: selectedChild,
+        onChildChanged: onChildChanged,
+        onSubmit: onSubmit,
+        navigateText: '등록 완료',
+      ),
+      bottomNavigationBar: CustomNavigationBar(
+        currentIndex: 1,
+        onTap: (index) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (_) => RootTab(initialTabIndex: index)),
+          );
+        },
+      ),
+    );
+  }
+}
