07658f0530f8d68af322e29d57ca791b11bcfafc
Layton <jihwan8807@naver.com>
2025-07-12 02:09:58 +0900

    ✨ [Refactor] Standardize token management methods to use SecureStorageKey for improved readability and maintainability

--- lib/common/utils/secure_storage_service.dart
+++ lib/common/utils/secure_storage_service.dart
@@ -1,17 +1,34 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+class SecureStorageKey {
+  static const accessToken = 'access_token';
+  static const refreshToken = 'refresh_token';
+}
+
 class SecureStorageService {
   static final FlutterSecureStorage _storage = FlutterSecureStorage();
 
-  static Future<void> saveToken(String token) async {
-    await _storage.write(key: 'accessToken', value: token);
+  static Future<void> saveAccessToken(String token) async {
+    await _storage.write(key: SecureStorageKey.accessToken, value: token);
   }
 
-  static Future<String?> getToken() async {
-    return await _storage.read(key: 'accessToken');
+  static Future<String?> getAccessToken() async {
+    return await _storage.read(key: SecureStorageKey.accessToken);
   }
 
-  static Future<void> deleteToken() async {
-    await _storage.delete(key: 'accessToken');
+  static Future<void> deleteAccessToken() async {
+    await _storage.delete(key: SecureStorageKey.accessToken);
+  }
+
+  static Future<void> saveRefreshToken(String token) async {
+    await _storage.write(key: SecureStorageKey.refreshToken, value: token);
+  }
+
+  static Future<String?> getRefreshToken() async {
+    return await _storage.read(key: SecureStorageKey.refreshToken);
+  }
+
+  static Future<void> deleteRefreshToken() async {
+    await _storage.delete(key: SecureStorageKey.refreshToken);
   }
 }
