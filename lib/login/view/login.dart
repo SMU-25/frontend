912d60cb6920e9e12c248cb7df8f10a255e62b47
Layton <jihwan8807@naver.com>
2025-07-14 12:27:50 +0900

    ✨ [Refactor] Simplify navigation after successful login by using named route

--- lib/login/view/login.dart
+++ lib/login/view/login.dart
@@ -45,12 +45,8 @@ class _LoginScreenState extends State<LoginScreen> {
           final token = res.data['result']['accessToken'];
           SecureStorageService.saveAccessToken(token);
           await SecureStorageService.saveAccessToken(token);
-
           if (!mounted) return;
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (ctx) => HomeScreen()),
-          );
+          Navigator.pushReplacementNamed(context, '/home');
         } else {
           if (!mounted) return;
           showErrorDialog(context: context, message: '로그인에 실패했습니다.');
