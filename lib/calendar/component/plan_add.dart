f6682af19ab95dd75f4c89dd8904fc7e3fa76b4c
choi0510 <yh733609@gmail.com>
2025-08-25 18:43:16 +0900

    ✨ [Feat] 캘린더 일정 생성 API 연동 및 UI 적용

--- lib/calendar/component/plan_add.dart
+++ lib/calendar/component/plan_add.dart
@@ -1,7 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:team_project_front/calendar/component/custom_text_input.dart';
 import 'package:team_project_front/calendar/model/plan.dart';
 import 'package:team_project_front/common/component/navigation_button.dart';
+import 'package:team_project_front/common/const/base_url.dart';
+import 'package:team_project_front/common/utils/secure_storage_service.dart';
+
 
 class PlanAdd extends StatefulWidget {
   final TextEditingController titleController;
@@ -34,18 +38,51 @@ class _PlanAddState extends State<PlanAdd> {
     }
   }
 
-  void onPressed () {
-    // final plan = Plan(
-    //   calendarId: widget.existingId ?? DateTime.now().millisecondsSinceEpoch,
-    //   title: widget.titleController.text,
-    //   content: widget.contentController.text,
-    //   date: widget.selectedDay,
-    //   recordDate: DateTime.now().toUtc(),
-    // );
+  void onPressed () async {
+    final accessToken = await SecureStorageService.getAccessToken();
 
-    // Navigator.of(context).pop(
-    //   plan,
-    // );
+    if (accessToken == null) {
+      print('AccessToken 없음!');
+      return;
+    }
+
+    final requestBody = {
+      "recordDate": DateTime.now().toIso8601String().substring(0, 10),
+      "scheduleDate": widget.selectedDay.toIso8601String().substring(0, 10),
+      "title": widget.titleController.text,
+      "content": widget.contentController.text,
+    };
+
+    final dio = Dio();
+    try {
+      final response = await dio.post(
+        '$base_URL/calendars',
+        data: requestBody,
+        options: Options(
+          headers: {'Authorization': 'Bearer $accessToken'},
+        ),
+      );
+
+      if (response.statusCode == 200 && response.data['isSuccess'] == true) {
+        final result = response.data['result'];
+
+        final newPlan = Plan.fromMap({
+          'calendarId': result['calendarId'],
+          'recordDate': result['recordDate'],
+          'scheduleDate': result['scheduleDate'],
+          'title': result['title'],
+          'content': result['content'],
+        });
+
+        Navigator.of(context).pop(newPlan);
+      } else {
+        print('일정 생성 실패: ${response.data['message']}');
+        Navigator.of(context).pop(null);
+      }
+    } catch (e) {
+      print('일정 생성 중 오류 발생: $e');
+      Navigator.of(context).pop(null);
+    }
   }
 
   @override
