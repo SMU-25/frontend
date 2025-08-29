da15b09b5096917b3f2775dcc8ad158ba3d87c86
Layton <jihwan8807@naver.com>
2025-08-29 11:57:49 +0900

    [Refactor] Remove unused date selection and chart section from HomeCamView

--- lib/homecam/component/home_cam_view.dart
+++ lib/homecam/component/home_cam_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_mjpeg/flutter_mjpeg.dart';
-import 'package:team_project_front/common/component/temperature_chart_widget.dart';
 import 'package:team_project_front/homecam/model/home_cam.dart';
 
 class HomeCamView extends StatefulWidget {
@@ -16,26 +15,11 @@ class HomeCamView extends StatefulWidget {
 class _HomeCamViewState extends State<HomeCamView> {
   DateTime selectedDate = DateTime.now();
 
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate,
-      firstDate: DateTime(2006),
-      lastDate: DateTime.now(),
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
+  final String selectedDateText = DateFormat(
+    'yyyy년 MM월 dd일',
+  ).format(DateTime.now());
   @override
   Widget build(BuildContext context) {
-    final String selectedDateText = DateFormat(
-      'yyyy년 MM월 dd일',
-    ).format(selectedDate);
-
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -52,47 +36,8 @@ class _HomeCamViewState extends State<HomeCamView> {
                     : const Center(child: Text("스트리밍 URL 없음")),
           ),
           const SizedBox(height: 12),
-          const Text(
-            '방 온도/습도 그래프',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          TextButton(
-            onPressed: () => _selectDate(context),
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.black,
-              textStyle: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(selectedDateText),
-                const SizedBox(width: 4),
-                const Icon(Icons.arrow_drop_down),
-              ],
-            ),
-          ),
-          const _ChartSectionWidget(chartType: ChartType.roomTemp),
-          const _ChartSectionWidget(chartType: ChartType.humidity),
         ],
       ),
-    );
-  }
-}
-
-class _ChartSectionWidget extends StatelessWidget {
-  const _ChartSectionWidget({required this.chartType});
-
-  final ChartType chartType;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      // TemperatureChartWidget(chartType: chartType),
     );
   }
 }
