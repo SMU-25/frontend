6ad571540880d4d5cce416ad7a1c8aac0afebbea
Layton <jihwan8807@naver.com>
2025-10-17 23:33:51 +0900

    refactor : alert api 요청 로직 data source로 분리

--- lib/features/home/data/datasources/alert_data_source.dart
+++ lib/features/home/data/datasources/alert_data_source.dart
@@ -1,9 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:team_project_front/core/network/dio_client.dart';
 import 'package:team_project_front/features/home/data/models/alert.dart';
 import 'package:team_project_front/features/home/data/models/notification.dart';
 import 'package:team_project_front/features/home/data/models/notification_response.dart';
 import 'package:team_project_front/features/home/data/models/page_result.dart';
 import 'package:team_project_front/features/home/data/models/paged_alert_response.dart';
+import 'package:team_project_front/features/home/domain/entities/alert_entity.dart';
 
 class AlertDataSource {
   final _dio = buildAuthedDio();
@@ -61,7 +63,18 @@ class AlertDataSource {
   }
 
   Future<bool> deleteAnnouncement(int id) async {
-    final res = await _dio.delete('/announcements/$id');
-    return res.statusCode == 200;
+    try {
+      final res = await _dio.delete('/announcements/$id');
+
+      if (res.statusCode == 200) {
+        return true;
+      } else {
+        throw Exception('삭제 실패 (HTTP ${res.statusCode})');
+      }
+    } on DioException catch (e) {
+      throw Exception('서버 요청 실패: ${e.message}');
+    } catch (e) {
+      throw Exception('공지 삭제 중 알 수 없는 오류 발생: $e');
+    }
   }
 }
